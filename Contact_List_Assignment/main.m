dc0e245327c3d88b489c014d85e05c63287cbd96
LuizRamos <luizramospe@hotmail.com>
2019-04-30 15:20:06 -0400

    First commit

---
+++ Contact_List_Assignment/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Contact_List_Assignment
+//
+//  Created by Luiz on 4/30/19.
+//  Copyright © 2019 Luiz. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[]) {
+    @autoreleasepool {
+        // insert code here...
+        NSLog(@"Hello, World!");
+    }
+    return 0;
+}
