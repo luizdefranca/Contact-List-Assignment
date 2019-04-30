71c3e81b1d806c23c1f8a5ac0da82f49cde29ac0
LuizRamos <luizramospe@hotmail.com>
2019-04-30 17:54:19 -0400

    Finished  task Bonus 1: Contact details (show command)

--- Contact_List_Assignment/ContactList.h
+++ Contact_List_Assignment/ContactList.h
@@ -15,6 +15,7 @@ NS_ASSUME_NONNULL_BEGIN
 - (instancetype)init;
 -(void)addContact:(Contact *)newContact;
 -(void)listContact;
+-(void)showContactWithNumber: (NSInteger) contactNumber;
 @end
 
 NS_ASSUME_NONNULL_END
--- Contact_List_Assignment/main.m
+++ Contact_List_Assignment/main.m
@@ -14,10 +14,19 @@
 int main(int argc, const char * argv[]) {
     
     @autoreleasepool {
-        NSString static *menuScreen = @"What would you like do next?\n" "new - Create a new contact\n" "list - List all contacts\n" "quit - Exit Application > ";
+        NSString static *menuScreen = @"What would you like do next?\n" "new - Create a new contact\n" "list - List all contacts\n" "show - Show detail of contact\n" "quit - Exit Application > ";
+        Contact *test = [[Contact alloc]initWithName:@"test" andPhone:@"6786"];
+        NSLog(@"%@", test);
         
         BOOL keepON = YES;
         ContactList *agenda = [[ContactList alloc] init];
+        
+        Contact *contact1 = [[Contact alloc] initWithName:@"Paul" andPhone:@"678686868"];
+        Contact *contact2 = [[Contact alloc] initWithName:@"Luiz" andPhone:@"7897979"];
+        Contact *contact3 = [[Contact alloc] initWithName:@"Ted" andPhone:@"678686"];
+        [agenda addContact:contact1];
+        [agenda addContact:contact2];
+        [agenda addContact:contact3];
         
         while (keepON) {
             NSString *option = [InputCollector inputForPrompt:menuScreen];
@@ -37,6 +46,17 @@ int main(int argc, const char * argv[]) {
             } else if ([option isEqualToString:@"list"]){
                 
                 [agenda listContact];
+            } else if ([option isEqualToString:@"show"]){
+                NSString *inputId = [InputCollector inputForPrompt: @"Type the contact id:"];
+                NSNumberFormatter *formatter = [[NSNumberFormatter alloc]init];
+                NSNumber *id = [formatter numberFromString:inputId];
+                if (id != nil) {
+                   
+                    [agenda showContactWithNumber:  [id integerValue]];
+                } else {
+                    NSLog(@"%@", @"It's not a valid number id");
+                }
+                
             }
         }
 
